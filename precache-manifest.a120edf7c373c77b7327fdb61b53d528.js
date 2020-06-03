071396722d83b8da20e9d31f3b4c66153fb2fbb6
Tommaso <tommaso.vsr@gmail.com>
2020-06-03 12:23:45 +0200

    Updates

---
+++ precache-manifest.a120edf7c373c77b7327fdb61b53d528.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf16c9fe0b0a9a72fa3b492bf03896ef",
+    "url": "/tportfolio/index.html"
+  },
+  {
+    "revision": "445801f1eadb6eabb433",
+    "url": "/tportfolio/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "98c69da1eb29e57d6788",
+    "url": "/tportfolio/static/css/main.aa32d6ed.chunk.css"
+  },
+  {
+    "revision": "445801f1eadb6eabb433",
+    "url": "/tportfolio/static/js/2.3bf80abd.chunk.js"
+  },
+  {
+    "revision": "1baa02233bbd827e3584e32091a75902",
+    "url": "/tportfolio/static/js/2.3bf80abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c69da1eb29e57d6788",
+    "url": "/tportfolio/static/js/main.50e4d343.chunk.js"
+  },
+  {
+    "revision": "b9ae2c069615bd019995",
+    "url": "/tportfolio/static/js/runtime-main.d9e8d04f.js"
+  },
+  {
+    "revision": "d87454532552b030d3a7bbef8f767f75",
+    "url": "/tportfolio/static/media/Mywork1.d8745453.png"
+  },
+  {
+    "revision": "dd2de89aa47cefb54c5f710d5c7129f6",
+    "url": "/tportfolio/static/media/jungle_background8.dd2de89a.jpg"
+  },
+  {
+    "revision": "3e1260c9e3a519d05c21e53ac99cd613",
+    "url": "/tportfolio/static/media/lobster.3e1260c9.jpg"
+  }
+]);
