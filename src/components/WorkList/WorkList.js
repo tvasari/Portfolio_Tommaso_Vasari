4921ffb233e73146137c6fade8c3726057070e7a
Tommaso <tommaso.vsr@gmail.com>
2020-06-12 14:41:23 +0200

    Update WorkList.js

--- src/components/WorkList/WorkList.js
+++ src/components/WorkList/WorkList.js
@@ -9,9 +9,9 @@ const db = [
 	{
 		titolo: 'Brain Rekon',
 		paragrafo: `Face recognition application with back-end functionalities. 
-								After registration, the user can detect a face on an image by submitting the image URL. 
-								Registered users are stored in a database which remembers the number of times a user made a search. 
-								The project was built using React.js for the front-end and node/express.js for the server side.`,
+			    After registration, the user can detect a face on an image by submitting the image URL. 
+			    Registered users are stored in a database which remembers the number of times a user made a search. 
+			    The project was built using React.js for the front-end and node/express.js for the server side.`,
 		immagine: mywork1,
 		appLink: 'https://brain-reckon.herokuapp.com',
 		frontLink: 'https://github.com/tvasari/brain_reckon',
