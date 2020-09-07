097a12510ff346991011272e223029feecabbc5e
Tommaso <tommaso.vsr@gmail.com>
2020-09-07 12:02:09 +0200

    cambiato nome progetto

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tportfolio/precache-manifest.decfd695a4abfedf9bcb46b6b054b576.js"
+  "tommasovasari.com/precache-manifest.decfd695a4abfedf9bcb46b6b054b576.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("tommasovasari.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
